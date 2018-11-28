baf48d5b2adfa868392a0344ddcc1c83934d201a
CSylvain <chrissylvain+gh@gmail.com>
2018-11-28 14:50:12 -0500

    use urllib3 exclusively
    fascinating how python3 code working one day starts throwing exceptions a few days later after what seemed to be minor package updates. granted, the network access functions in the previous version were more 'python2-like' than python3-like. it's now refactored to be entirely python3-like. i see now why virtual environments are popular and why configuration issues are slated to get more 'scrutable' error messages

--- techrmsensorlogger-github.py
+++ techrmsensorlogger-github.py
@@ -6,55 +6,59 @@
 # take data reported over wifi from RGBClock (Elektor.com kit w/BME280 BoB)
 #   and report measurements to Thingspeak channel
 #
-# code structure inspired by templogger.py by Matt Hawkins 20/06/2015
+# code structure inspired by templogger.py by Matt Hawkins 2015-06-20
 #   https://www.raspberrypi-spy.co.uk/2015/06/basic-temperature-logging-to-the-internet-with-raspberry-pi/
 #
 # code developed with many references to https://docs.python.org/3.6/library/index.html
 #   and https://www.w3schools.com/python/ using Visual Studio Code on Linux
 #
-# CSylvain KB3CS  2018-11-14
+# CSylvain KB3CS  2018-11-14 initial version
+# CSylvain KB3CS  2018-11-25 use urllib3 exclusively
 #-----------------------------------------------------------------------------
 
 import time
 import sys
 import traceback
-import urllib
-import urllib.request
+import certifi
+import urllib3
 
 ################# Configuration Defaults #################
-THINGSPEAKKEY = 'ABCDEFGHIJKLMNOP'
+THINGSPEAKKEY = 'ABCDEFGHIJKLMNOPQRSTUV'
 THINGSPEAKURL = 'https://api.thingspeak.com/update'
 RGBCLOCKADDR  = '192.168.1.105:81'
 DEBUG         = 0
 ##########################################################
 
-def sendData (url,chkey,fn1,fn2,fn3,t,p,h):
+def sendData (hpm,url,chkey,fn1,fn2,fn3,t,p,h):
     # send sensor data
     values = { 'api_key' : chkey,
                'field1' : t,
                'field2' : p,
                'field3' : h }
-    pd = urllib.parse.urlencode(values)
-    if DEBUG:
-        print (pd)
     
     ts = time.strftime("%FT%H:%M:%S%z")
     try:
-        with urllib.request.urlopen(url, bytes(pd,'ascii')) as f:
-            r = f.read().decode('utf-8')
-            f.close()
+        r = hpm.request('GET', url, fields=values)
+        rdd = r.data.decode('utf-8')
+        if DEBUG:
+            print (rdd)
+        r.close()
 
     except Exception as e:
         print (ts, "* Exception ", repr(e))
         return 1
     
-    print (ts, '/', fn1, t, '/', fn2, p, '/', fn3,  h, '/ Response', r)
+    print (ts, '/', fn1, t, '/', fn2, p, '/', fn3,  h, '/ Response', rdd)
     return 0
 
 
 def main ():
     global THINGSPEAKKEY
     global THINGSPEAKURL
+
+    http = urllib3.PoolManager(retries=False, timeout=5.0) # http
+    https = urllib3.PoolManager(retries=False, timeout=5.0,
+                                cert_reqs='CERT_REQUIRED', ca_certs=certifi.where()) # https    
 
     try:
         if DEBUG:
@@ -67,10 +71,11 @@ def main ():
 
             ts = time.strftime("%FT%H:%M:%S%z")
             try:           
-                with urllib.request.urlopen(s) as f:
-                    r = f.read().decode('utf-8')
+                r = http.request('GET', s)
+                rdd = r.data.decode('utf-8')
+                r.close()
                 if DEBUG > 2:
-                    print(ts, r)
+                    print(ts, rdd)
             
             except Exception as e:
                 print (ts, "* Exception ", repr(e))
@@ -78,7 +83,7 @@ def main ():
                 time.sleep(2*60)  # wait 2 mins
                 continue
 
-            d = r.split(',')
+            d = rdd.split(',')
 
             sobj = slice(0, len(d[1])-2)
             d[1] = d[1][sobj]  # numeric part of '23.36*C'
@@ -92,7 +97,7 @@ def main ():
                 print('pressure    = ', d[2])
                 print('humidity    = ', d[3])
 
-            rv = sendData(THINGSPEAKURL,THINGSPEAKKEY,
+            rv = sendData(https,THINGSPEAKURL,THINGSPEAKKEY,
                             'temperature','pressure','humidity',d[1],d[2],d[3])
 
             sys.stdout.flush()
@@ -105,6 +110,9 @@ def main ():
         print ("something went very wrong")
         print ("* Exception ", repr(e))
         traceback.print_exc(file=sys.stdout)
+        return 1
+    
+    return 0 # notreached
 
 if __name__ == "__main__":
     main()
